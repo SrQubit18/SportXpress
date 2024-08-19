4e6c0794659c8cbc5451fefd37c69cf2cc9a12ee
SrQubit18 <srqubit8@gmail.com>
2024-08-18 22:48:22 -0300

    jghkggkjgjk

--- mercado/mercado/settings.py
+++ mercado/mercado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cd050topi$n!ahjf9z=%bl+n+%lh8esgssm(zj+12%b@ie3bb*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
