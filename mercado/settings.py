c772b8bb8c9ac351bf619f7cc796e524c36d9759
SrQubit18 <srqubit8@gmail.com>
2024-08-18 22:26:03 -0300

    asdasdasd

--- mercado/settings.py
+++ mercado/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cd050topi$n!ahjf9z=%bl+n+%lh8esgssm(zj+12%b@ie3bb*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
