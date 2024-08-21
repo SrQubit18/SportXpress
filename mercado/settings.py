b348298d8706991eca15402b6d6a4f14d16dd916
SrQubit18 <srqubit8@gmail.com>
2024-08-21 05:11:07 -0300

    jjjkjk

--- mercado/settings.py
+++ mercado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cd050topi$n!ahjf9z=%bl+n+%lh8esgssm(zj+12%b@ie3bb*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["SportXpress.pythonanywhere.com"]
 
 
 # Application definition
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'mercado.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/home/SportXpress/SportXpress/carrito/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = ['/home/SportXpress/SportXpress/carrito/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
